77a9794ec1229af05b1cd25886f87a5420df805e
LaperCode <lapper2005@gmail.com>
2024-08-01 15:42:28 +0700

    add file

---
+++ GITHUB/main1.java
@@ -0,0 +1,8 @@
+//package JAVA;
+
+public class main1 {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!! 2");
+        System.out.println("\nMy name is Lap");
+    }
+}
